be146866d643ec3f525eec3a8bdfbfe108c6a076
Hikareel <filipst166@gmail.com>
2022-01-12 19:58:54 +0100

    Show Cards on table

---
+++ loadCards.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+import pygame.image
+
+colors = ['C', 'D', 'H', 'S']
+figures = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+cards = []
+ranCardsPlayer = []
+ranCardsComputer = []
+class Card:
+    def __init__(self, color, figure, image):
+        self.color = color
+        self.figure = figure
+        self.image = image
+        self.state = "Deck"
+
+
+def loadCards():
+    for color in range(len(colors)):
+        for figure in range(len(figures)):
+            cards.append(Card(colors[color], figures[figure], pygame.image.load("D:\\Projekty\\MakaoPython\\PNG\\" + figures[figure] + colors[color] + ".png")))
+
+def randomCards(forWho):
+    for i in (1, 2, 3, 4, 5):
+        a = random.randint(0, 51)
+        while cards[a].state != "Deck":
+            a = random.randint(0, 51)
+        ranCardsPlayer.append(cards[a]) if forWho == "Player" else ranCardsComputer.append(cards[a])
+        cards[a].state = forWho
+
--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import pygame
+from loadCards import *
 
+a = True
 pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.WINDOWRESIZED)
+screen = pygame.display.set_mode((1280, 720), 0, 32)
+loadCards()
+randomCards("Player")
+randomCards("Computer")
+
+def showCards(forWho):
+    if forWho == ranCardsPlayer:
+        x = 350
+        y = 600
+        for i in range(len(forWho)):
+            screen.blit(forWho[i].image, (x, y))
+            x += 100
+    if forWho == ranCardsComputer:
+        x = 1200
+        y = 50
+        for i in range(len(forWho)):
+            screen.blit(forWho[i].image, (x, y))
+            y+=100
+
+
+while a:
+    screen.fill((36, 92, 25))
+    showCards(ranCardsPlayer)
+    showCards(ranCardsComputer)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            a = False
+    pygame.display.update()
+
 
